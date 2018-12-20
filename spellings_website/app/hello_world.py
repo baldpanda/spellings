885e508460bf02e22660278ce5a2fbdccd4c13e1
baldpanda <patrick.j.callery@bath.edu>
2018-12-20 21:27:37 +0000

    Adding html templates for home page and spelling page and passing basic data to templates

--- spellings_website/app/hello_world.py
+++ spellings_website/app/hello_world.py
@@ -1,17 +1,17 @@
-#hello_word.py
-
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+sample_sentence = ("The _ _ _ sat on the mat.")
+
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return '<h1>Bald Panda!</h1>'
+    return render_template('home.html')
 
 @app.route('/spellings')
 def spelling_page():
-    return 'Placeholder for spellings'
+    return render_template('spellings.html', sample_sentence = sample_sentence)
 
 if __name__ == '__main__':
     app.run(debug = True)
