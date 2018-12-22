504e5bfdcf2032494ee47433b622d4cc6eca16ee
baldpanda <patrick.j.callery@bath.edu>
2018-12-22 18:12:47 +0000

    adding registration form and some of the login form

--- spellings_website/app/hello_world.py
+++ spellings_website/app/hello_world.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '5b15f64dade99ceb17ee15983fa4bc80'
 sample_sentence = ("The _ _ _ sat on the mat.")
 
 
@@ -12,6 +14,16 @@ def home():
 @app.route('/spellings')
 def spelling_page():
     return render_template('spellings.html', sample_sentence = sample_sentence)
+
+@app.route('/registration')
+def registration_page():
+    form = RegistrationForm()
+    return render_template('register.html', sample_sentence = sample_sentence, form = form)
+
+@app.route('/login')
+def login_page():
+    form = LoginForm()
+    return render_template('login.html', sample_sentence = sample_sentence,form = form)
 
 if __name__ == '__main__':
     app.run(debug = True)
