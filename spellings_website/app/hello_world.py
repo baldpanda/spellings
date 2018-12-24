e151c16f1ee02b3ac5f54cfe231fed94dabad941
baldpanda <patrick.j.callery@bath.edu>
2018-12-24 13:58:24 +0000

    adding a sqlite db and using sqlalchemy as an ORM

--- spellings_website/app/hello_world.py
+++ spellings_website/app/hello_world.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5b15f64dade99ceb17ee15983fa4bc80'
-sample_sentence = ("The _ _ _ sat on the mat.")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(20), unique = True, nullable = False)
+    email = db.Column(db.String(120), unique = True, nullable = False)
+    password = db.Column(db.String(60), nullable = False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
+
+sample_sentence = ("The _ _ _ sat on the mat.")
 
 @app.route('/')
 @app.route('/home')
