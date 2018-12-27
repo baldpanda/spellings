5da722342f1adddee2d94daca139056c399c9a2e
baldpanda <patrick.j.callery@bath.edu>
2018-12-27 09:13:23 +0000

    adding functionalilty to login and logout and adding route for adding sentences

--- spellings_website/app/baldpanda_site/routes.py
+++ spellings_website/app/baldpanda_site/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, request
+from flask_login import login_user, current_user, logout_user, login_required
 from baldpanda_site import app, db, bcrypt
 from baldpanda_site import app
 from baldpanda_site.forms import RegistrationForm, LoginForm
@@ -18,6 +19,8 @@ def spelling_page():
 @app.route('/registration', methods = ['GET', 'POST'])
 def registration_page():
     form = RegistrationForm()
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
@@ -30,10 +33,24 @@ def registration_page():
 @app.route('/login', methods = ['GET', 'POST'])
 def login_page():
     form = LoginForm()
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     if form.validate_on_submit():
-        if form.email.data == 'pcallery@baldpanda.com' and form.password.data == 'password':
-            flash('Successful Login', 'success')
-            return redirect(url_for('home'))
+        user = User.query.filter_by(email = form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember = form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash('Login Unsuccessful', 'danger')
     return render_template('login.html', sample_sentence = sample_sentence,form = form)
+
+@app.route('/logout')
+def logout_page():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route('/sentence_adder')
+@login_required
+def sentence_adder():
+    return render_template('sentence_adder.html', title = 'sentence_adder')
