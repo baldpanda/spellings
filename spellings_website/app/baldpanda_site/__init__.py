29aedaf6531eae0c0190a92b0ad9bfbe906848c0
baldpanda <patrick.j.callery@bath.edu>
2019-02-01 20:10:29 +0000

    moving some of the secret information over to environment variables and creating integrating config file with site init file

--- spellings_website/app/baldpanda_site/__init__.py
+++ spellings_website/app/baldpanda_site/__init__.py
@@ -1,23 +1,17 @@
+from baldpanda_site.config import Config
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
-import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5b15f64dade99ceb17ee15983fa4bc80'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config.from_object(Config)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'users.login_page'
 login_manager.login_message_category = 'info'
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('PANDA_EMAIL')
-app.config['MAIL_PASSWORD'] = os.environ.get('PANDA_PASSWORD')
 mail = Mail(app)
 
 from baldpanda_site.users.routes import users
