8b83a22d0b4d1d5193d2a56fa9e6c4bc7555d870
baldpanda <patrick.j.callery@bath.edu>
2019-02-07 23:13:47 +0000

    adding 500 error code

--- spellings_website/app/baldpanda_site/errors/handlers.py
+++ spellings_website/app/baldpanda_site/errors/handlers.py
@@ -5,3 +5,7 @@ errors = Blueprint('errors', __name__)
 @errors.app_errorhandler(404)
 def error_404(error):
     return render_template('errors/404.html'), 404
+
+@errors.app_errorhandler(500)
+def error_404(error):
+    return render_template('errors/500.html'), 500
