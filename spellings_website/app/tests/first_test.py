4cfb3e82d3b79c5510c14ff512eedbc6acffd101
baldpanda <patrick.j.callery@bath.edu>
2019-02-09 20:46:18 +0000

    linking to flask testing skeleton

--- spellings_website/app/tests/first_test.py
+++ spellings_website/app/tests/first_test.py
@@ -2,7 +2,12 @@ import unittest
 
 class SimpleTest(unittest.TestCase):
     def test_equality(self):
-        self.assertEqual(1,1)
+        self.assertEqual(1,0)
+
+
+### Need to make some sort of flask test infrastructure
+###link to test skeleton
+###http://flask.pocoo.org/docs/0.12/testing/
 
 if __name__ =='__main__':
     unittest.main()
