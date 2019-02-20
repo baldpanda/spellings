cfd9ffff90cdb2aab1768817b59427c9cc12a7ce
baldpanda <patrick.j.callery@bath.edu>
2019-02-20 21:48:06 +0000

    removing more spaces in arguements to make code more pep-8

--- spellings_website/app/baldpanda_site/models.py
+++ spellings_website/app/baldpanda_site/models.py
@@ -8,11 +8,11 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(20), unique = True, nullable = False)
-    email = db.Column(db.String(120), unique = True, nullable = False)
-    password = db.Column(db.String(60), nullable = False)
-    sentences = db.relationship('Sentence', backref = 'author', lazy = True)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    sentences = db.relationship('Sentence', backref='author', lazy=True)
 
     def get_reset_token(self, expires_sec=1800):
         s = Serializer(current_app.config['SECRET_KEY'], expires_sec)
@@ -31,9 +31,9 @@ class User(db.Model, UserMixin):
         return f"User('{self.username}', '{self.email}')"
 
 class Sentence(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key = True)
-    sentence = db.Column(db.String(100), nullable = False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
+    id = db.Column(db.Integer, primary_key=True)
+    sentence = db.Column(db.String(100), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
         return f"User('{self.sentence}')"
