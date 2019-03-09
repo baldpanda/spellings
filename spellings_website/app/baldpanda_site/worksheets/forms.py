25eb3808725ffa1fd3d4c6cd5caa464919fa72d6
baldpanda <patrick.j.callery@bath.edu>
2019-03-09 22:13:57 +0000

    Cleaning up forms for worksheets using pylint

--- spellings_website/app/baldpanda_site/worksheets/forms.py
+++ spellings_website/app/baldpanda_site/worksheets/forms.py
@@ -1,12 +1,18 @@
+"""Forms for adding sentences to db and
+submitting required words to worksheet
+"""
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 class NewSentence(FlaskForm):
+    """Form for adding new sentence to db"""
     sentence = StringField('Sentence', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 class WordsForSheet(FlaskForm):
-    words = StringField('Please enter the required words, seperating them using a comma.'
+    """Form for submitting required words for worksheet"""
+    words = StringField('Please enter the required words, seperating them using a comma.'\
     , validators=[DataRequired()])
     submit = SubmitField('Make Worksheet')
