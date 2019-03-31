43ca8bdeb6207bebf0800b26c965ee9d8b14509b
baldpanda <patrick.j.callery@bath.edu>
2019-03-31 22:04:29 +0100

    adding docstring and module string to users utils module

--- spellings_website/app/baldpanda_site/users/utils.py
+++ spellings_website/app/baldpanda_site/users/utils.py
@@ -1,8 +1,11 @@
+"""Utility methods to be used by route methods for users"""
+
 from flask import url_for
 from flask_mail import Message
 from baldpanda_site import mail
 
 def send_reset_email(user):
+    """send password reset email from baldpanda email address"""
     token = user.get_reset_token()
     msg = Message('Password Reset Request', sender='baldpanda94@gmail.com', recipients=[user.email])
     msg.body = f'''To reset your password please visit the following link:
